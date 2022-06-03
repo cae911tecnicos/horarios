3bd295ef2ecca73f034a761ffbc086fef2bd1c2c
Fernando Saucedo <57181462+saucedoinfo@users.noreply.github.com>
2022-06-03 11:45:01 -0300

    202206031144

---
+++ assets/js/feriados.js
@@ -0,0 +1,36 @@
+export const feriados = [
+  {
+    dia: "17-06-2022",
+    acontecimiento:
+      "17. Paso a la Inmortalidad del Gral. Don Martín Miguel de Güemes",
+  },
+  {
+    dia: "20-06-2022",
+    acontecimiento: "20. Paso a la Inmortalidad del Gral. Manuel Belgrano",
+  },
+  {
+    dia: "09-07-2022",
+    acontecimiento: "9. Día de la Independencia",
+  },
+  {
+    dia: "15-09-2022",
+    acontecimiento:
+      "15. Paso a la Inmortalidad del Gral. José de San Martín (17/8)",
+  },
+  {
+    dia: "07-10-2022",
+    acontecimiento: "7. Feriado con fines turísticos",
+  },
+  {
+    dia: "10-10-2022",
+    acontecimiento: "10. Día del Respeto a la Diversidad Cultural (12/10)",
+  },
+  {
+    dia: "21-11-2022",
+    acontecimiento: "21. Feriado con fines turísticos",
+  },
+  {
+    dia: "25-12-2022",
+    acontecimiento: "25. Navidad",
+  },
+];
