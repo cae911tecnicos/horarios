779a56aadb8055a172b5be8dfeb3d6116b94bf70
Fernando Saucedo <57181462+saucedoinfo@users.noreply.github.com>
2022-06-12 17:27:30 -0300

    202206121727

--- assets/js/lib/ciclos.js
+++ assets/js/lib/ciclos.js
@@ -4,67 +4,67 @@ import { personalAyudantia } from "../personal/personal-ayudantia.js";
 // CICLOS DIVISION AREA TECNIC Y SECCION INFORMES JUDICIALES -----------------------------------------
 //Estos  ciclos estan pensado para que una persona trabaje minimamente 3 veces por semana.
 // Cada ciclo dura un numero N de semanas, donde N es igual al numero de personal disponible.
-export const ciclo_2_tecnicos = [
+export const ciclo_tecnicos_2 = [
   [1, 2, 1, 2, 1, 2, 2],
   [1, 2, 1, 2, 1, 2, 2],
 ];
-export const ciclo_3_tecnicos = [
+export const ciclo_tecnicos_3 = [
   [1, 3, 2, 1, 3, 2, 2],
   [2, 1, 3, 2, 1, 2, 2],
 ];
-export const ciclo_4_tecnicos = [
+export const ciclo_tecnicos_4 = [
   [1, 3, 1, 4, 2, 4, 4],
   [2, 4, 2, 3, 1, 4, 4],
 ];
-export const ciclo_5_tecnicos = [
+export const ciclo_tecnicos_5 = [
   [12, 5, 23, 4, 13, 4, 4],
   [3, 4, 1, 5, 2, 4, 4],
 ];
-export const ciclo_6_tecnicos = [
+export const ciclo_tecnicos_6 = [
   [12, 34, 56, 32, 56, 4, 4],
   [56, 2, 1, 4, 13, 4, 4],
 ];
 
 // CICLOS DIVISION AYUDANTIA  -----------------------------------------------------------------------
-export const ciclo_2_ayudantia = [
+export const ciclo_ayudantia_2 = [
   [1, 2, 1, 2, 1, 2, 2],
   [1, 2, 1, 2, 1, 2, 2],
 ];
-export const ciclo_3_ayudantia = [
+export const ciclo_ayudantia_3 = [
   [12, 12, 12, 12, 12, 1, 1],
   [3, 3, 3, 3, 3, 1, 1],
 ];
-export const ciclo_4_ayudantia = [
+export const ciclo_ayudantia_4 = [
   [12, 41, 34, 23, 12, 4, 4],
   [3, 2, 1, 4, 3, 4, 4],
 ];
-export const ciclo_5_ayudantia = [
+export const ciclo_ayudantia_5 = [
   [12, 45, 23, 51, 34, 1, 1],
   [3, 1, 4, 2, 5, 1, 1],
 ];
-export const ciclo_6_ayudantia = [
+export const ciclo_ayudantia_6 = [
   [12, 34, 56, 32, 56, 4, 4],
   [56, 2, 1, 4, 13, 4, 4],
 ];
 
 // FUNCIONES PARA SECCION INFORMES JUDICIALES
-export const ciclo_2_informes_judiciales = [
+export const ciclo_informes_judiciales_2 = [
   [1, 2, 1, 2, 1, 2, 2],
   [1, 2, 1, 2, 1, 2, 2],
 ];
-export const ciclo_3_informes_judiciales = [
+export const ciclo_informes_judiciales_3 = [
   [1, 3, 2, 1, 3, 2, 2],
   [2, 1, 3, 2, 1, 2, 2],
 ];
-export const ciclo_4_informes_judiciales = [
+export const ciclo_informes_judiciales_4 = [
   [1, 3, 1, 4, 2, 4, 4],
   [2, 4, 2, 3, 1, 4, 4],
 ];
-export const ciclo_5_informes_judiciales = [
+export const ciclo_informes_judiciales_5 = [
   [12, 5, 23, 4, 13, 4, 4],
   [3, 4, 1, 5, 2, 4, 4],
 ];
-export const ciclo_6_informes_judiciales = [
+export const ciclo_informes_judiciales_6 = [
   [12, 34, 56, 32, 56, 4, 4],
   [56, 2, 1, 4, 13, 4, 4],
 ];
@@ -96,12 +96,12 @@ export const rotacion = (arr, personal) => {
     return arr;
   }
 };
+
 // Funcion para completar y generar los ciclos
 const ciclos = (ciclo, personal) => {
   for (let a = 0; a < 2; a++) {
     for (let i = 0; i < 7 * (personal - 1); i++) {
       let aRotar = ciclo[a][i];
-
       ciclo[a].push(rotacion(aRotar, personal));
     }
   }
@@ -112,44 +112,54 @@ export const cicloDelDia = (personal, numero) => {
   if (personal == personalAyudantia) {
     let ciclo =
       numero == 6
-        ? ciclos(ciclo_6_ayudantia, 6)
+        ? ciclos(ciclo_ayudantia_6, 6)
         : numero == 5
-        ? ciclos(ciclo_5_ayudantia, 5)
+        ? ciclos(ciclo_ayudantia_5, 5)
         : numero == 4
-        ? ciclos(ciclo_4_ayudantia, 4)
+        ? ciclos(ciclo_ayudantia_4, 4)
         : numero == 3
-        ? ciclos(ciclo_3_ayudantia, 3)
+        ? ciclos(ciclo_ayudantia_3, 3)
         : numero == 2
-        ? ciclos(ciclo_2_ayudantia, 2)
+        ? ciclos(ciclo_ayudantia_2, 2)
         : "No existe ese ciclo";
     return ciclo;
   } else if (personal == personalInformesJudiciales) {
     let ciclo =
       numero == 6
-        ? ciclos(ciclo_6_informes_judiciales, 6)
+        ? ciclos(ciclo_informes_judiciales_6, 6)
         : numero == 5
-        ? ciclos(ciclo_5_informes_judiciales, 5)
+        ? ciclos(ciclo_informes_judiciales_5, 5)
         : numero == 4
-        ? ciclos(ciclo_4_informes_judiciales, 4)
+        ? ciclos(ciclo_informes_judiciales_4, 4)
         : numero == 3
-        ? ciclos(ciclo_3_informes_judiciales, 3)
+        ? ciclos(ciclo_informes_judiciales_3, 3)
         : numero == 2
-        ? ciclos(ciclo_2_informes_judiciales, 2)
+        ? ciclos(ciclo_informes_judiciales_2, 2)
         : "No existe ese ciclo";
     return ciclo;
   } else {
     let ciclo =
       numero == 6
-        ? ciclos(ciclo_6_tecnicos, 6)
+        ? ciclos(ciclo_tecnicos_6, 6)
         : numero == 5
-        ? ciclos(ciclo_5_tecnicos, 5)
+        ? ciclos(ciclo_tecnicos_5, 5)
         : numero == 4
-        ? ciclos(ciclo_4_tecnicos, 4)
+        ? ciclos(ciclo_tecnicos_4, 4)
         : numero == 3
-        ? ciclos(ciclo_3_tecnicos, 3)
+        ? ciclos(ciclo_tecnicos_3, 3)
         : numero == 2
-        ? ciclos(ciclo_2_tecnicos, 2)
+        ? ciclos(ciclo_tecnicos_2, 2)
         : "No existe ese ciclo";
     return ciclo;
   }
 };
+
+// este tiene un error
+console.log(cicloDelDia(ciclo_ayudantia_5, 5))
+
+// Este no tiene error
+//console.error(ciclos(ciclo_ayudantia_5, 5))
+
+//console.log(rotacion(ciclo_ayudantia_5[1],5))
+
+
