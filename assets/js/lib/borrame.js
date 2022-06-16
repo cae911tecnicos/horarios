e3a1ea3a0f838ce9b30e3fd9fd5fa6ab9408bc17
Fernando Saucedo <57181462+saucedoinfo@users.noreply.github.com>
2022-06-16 14:33:39 -0300

    202206161433

--- assets/js/lib/borrame.js
+++ assets/js/lib/borrame.js
@@ -1,7 +1,54 @@
+const ciclo_tecnicos_2 = [
+  [1, 2, 1, 2, 1, 2, 2],
+  [1, 2, 1, 2, 1, 2, 2],
+];
+const ciclo_tecnicos_3 = [
+  [1, 3, 2, 1, 3, 2, 2],
+  [2, 1, 3, 2, 1, 2, 2],
+];
 const ciclo_tecnicos_4 = [
   [1, 3, 1, 4, 2, 4, 4],
   [2, 4, 2, 3, 1, 4, 4],
 ];
+const ciclo_tecnicos_5 = [
+  [12, 5, 23, 4, 13, 4, 4],
+  [3, 4, 1, 5, 2, 4, 4],
+];
+const ciclo_tecnicos_6 = [
+  [12, 34, 56, 32, 56, 4, 4],
+  [56, 2, 1, 4, 13, 4, 4],
+];
+// Funcion para completar el ciclo
+const rotacion = (arr, num) => {
+  let aux_1;
+  let aux_2;
+  arr = String(arr);
+  if (arr.length === 2) {
+    arr = arr.split("");
+    if (arr.length === 1) {
+      arr.unshift(0);
+    }
+
+    let aa = Number(arr[0]);
+    let bb = Number(arr[1]);
+
+    aux_1 = aa + 1;
+    aux_2 = bb + 1;
+
+    let a = arr !== aa || aux_1 < aa ? arr + 1 : (arr = 1);
+    let b = arr !== bb || aux_2 < bb ? arr + 1 : (arr = 1);
+
+    arr = `${a}${b}`;
+    arr = Number(arr);
+
+    return arr;
+  } else {
+    arr = Number(arr);
+    aux_1 = arr + 1;
+    let resultado = arr !== num || aux_1 < num ? arr + 1 : (arr = 1);
+    return resultado;
+  }
+};
 
 // Funcion para completar y generar los ciclos
 const ciclos = (ciclo, num) => {
@@ -16,32 +63,28 @@ const ciclos = (ciclo, num) => {
   // APARTE A
   for (let semana = 0; semana < num; semana++) {
     for (let i = 0; i < 7; i++) {
-      a = auxA[i] + 1;
-      b = auxA[i] !== num || a < num ? auxA[i] + 1 : (auxA[i] = 1);
-
-      auxA.push(b);
+      let posicion = rotacion(auxA[i], num);
+      auxA.push(posicion);
     }
     auxA.splice(0, 7);
     parteA = [...parteA, ...auxA];
   }
-  parteA.splice(28, 7);
+  parteA.splice(num * 7, 7);
   // APARTE B
   for (let semana = 0; semana < num; semana++) {
     for (let i = 0; i < 7; i++) {
-      a = auxB[i] + 1;
-      b = auxB[i] !== num || a < num ? auxB[i] + 1 : (auxB[i] = 1);
-
-      auxB.push(b);
+      let posicion = rotacion(auxB[i], num);
+      auxB.push(posicion);
     }
     auxB.splice(0, 7);
     parteB = [...parteB, ...auxB];
   }
-  parteB.splice(28, 7);
+  parteB.splice(num * 7, 7);
 
-  cicloCompleto.push(parteA)
-  cicloCompleto.push(parteB)
+  cicloCompleto.push(parteA);
+  cicloCompleto.push(parteB);
 
   return cicloCompleto;
 };
 
-console.error(ciclos(ciclo_tecnicos_4, 4));
+console.log(ciclos(ciclo_tecnicos_4, 4));
