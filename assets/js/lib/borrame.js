6ac563aed54aa641ea0124c90ecefd5f5ecd4d41
Fernando Saucedo <57181462+saucedoinfo@users.noreply.github.com>
2022-06-16 13:05:56 -0300

    202206161305

--- assets/js/lib/borrame.js
+++ assets/js/lib/borrame.js
@@ -5,25 +5,41 @@ const ciclo_tecnicos_4 = [
 
 // Funcion para completar y generar los ciclos
 const ciclos = (ciclo, num) => {
-  let cicloCompleto = [];
-  let aux = JSON.parse(JSON.stringify(ciclo[0]));
+  let auxA = JSON.parse(JSON.stringify(ciclo[0]));
+  let auxB = JSON.parse(JSON.stringify(ciclo[1]));
   let a = [];
   let b = [];
-  cicloCompleto = [...aux];
-  console.warn(cicloCompleto);
+  let parteA = [...auxA];
+  let parteB = [...auxB];
+  let cicloCompleto = [];
 
+  // APARTE A
   for (let semana = 0; semana < num; semana++) {
     for (let i = 0; i < 7; i++) {
-      a = aux[i] + 1;
-      b = aux[i] !== num || a < num ? aux[i] + 1 : (aux[i] = 1);
+      a = auxA[i] + 1;
+      b = auxA[i] !== num || a < num ? auxA[i] + 1 : (auxA[i] = 1);
 
-      aux.push(b);
+      auxA.push(b);
     }
-    aux.splice(0, 7);
-    cicloCompleto = [...cicloCompleto, ...aux];
-    console.warn(aux);
+    auxA.splice(0, 7);
+    parteA = [...parteA, ...auxA];
   }
-  cicloCompleto.splice(28, 7);
+  parteA.splice(28, 7);
+  // APARTE B
+  for (let semana = 0; semana < num; semana++) {
+    for (let i = 0; i < 7; i++) {
+      a = auxB[i] + 1;
+      b = auxB[i] !== num || a < num ? auxB[i] + 1 : (auxB[i] = 1);
+
+      auxB.push(b);
+    }
+    auxB.splice(0, 7);
+    parteB = [...parteB, ...auxB];
+  }
+  parteB.splice(28, 7);
+
+  cicloCompleto.push(parteA)
+  cicloCompleto.push(parteB)
 
   return cicloCompleto;
 };
