14dc13d0b319beb89d6b6c61792c33ea29548372
911tecnicos <cae911tecnicos@gmail.com>
2022-07-12 08:36:01 -0300

    202212070835

--- assets/js/personal/personal-division-area-tecnica.js
+++ assets/js/personal/personal-division-area-tecnica.js
@@ -4,35 +4,31 @@ export let personalTecnico = [
     nombre: "Fernando",
     apellido: "Saucedo",
     situacion: "Licencia",
-    inicioSituacion: "21-06-2022",
-    finSituacion: "04-07-2022",
+    inicioSituacion: "03-05-2022",
+    finSituacion: "30-05-2022",
   },
-  {
-    nombre: "Fernando",
-    apellido: "Saucedo",
-    situacion: "Licencia",
-    inicioSituacion: "14-03-2022",
-    finSituacion: "02-04-2022",
-  },
+
   {
     nombre: "Gabriel",
     apellido: "Knuttzen",
     situacion: "Licencia",
-    inicioSituacion: "03-05-2022",
-    finSituacion: "30-05-2022",
+
+    inicioSituacion: "21-06-2022",
+    finSituacion: "04-07-2022",
   },
   {
     nombre: "Milton",
     apellido: "Gerometta",
     situacion: "Licencia",
-    inicioSituacion: "18-02-2022",
-    finSituacion: "05-04-2022",
+    inicioSituacion: "22-04-2022",
+    finSituacion: "01-05-2022",
   },
   {
     nombre: "Mauricio",
     apellido: "Garigliano",
     situacion: "Licencia",
-    inicioSituacion: "22-04-2022",
-    finSituacion: "01-05-2022",
+
+    inicioSituacion: "18-02-2022",
+    finSituacion: "05-04-2022",
   },
 ];
