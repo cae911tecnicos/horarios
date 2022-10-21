d9199bda9eafed02128075591cadb7f09a2c37e0
911tecnicos <cae911tecnicos@gmail.com>
2022-10-21 09:30:20 -0300

    cambio de gero con sauce 26 al 27 del 10

--- assets/js/horario-manual/hm__div-area-tecnica.js
+++ assets/js/horario-manual/hm__div-area-tecnica.js
@@ -1,177 +1,187 @@
 export const manual_area_tecnica = [
+  {
+    dia: "29-08-2022",
+    maniana: "Galvan ",
+    tarde: "Knuttzen ",
+  },
+  {
+    dia: "30-08-2022",
+    maniana: "Saucedo ",
+    tarde: "Gerometta ",
+  },
+  {
+    dia: "31-08-2022",
+    maniana: "Galvan ",
+    tarde: "Knuttzen ",
+  },
+  {
+    dia: "01-09-2022",
+    maniana: "Gerometta ",
+    tarde: "Saucedo ",
+  },
+  {
+    dia: "02-09-2022",
+    maniana: "Knuttzen ",
+    tarde: "Galvan ",
+  },
+  {
+    dia: "03-09-2022",
+    maniana: "Gerometta ",
+    tarde: "Gerometta ",
+  },
+  {
+    dia: "04-09-2022",
+    maniana: "Gerometta ",
+    tarde: "Gerometta ",
+  },
+  {
+    dia: "05-09-2022",
+    maniana: "Knuttzen ",
+    tarde: "Saucedo ",
+  },
+  {
+    dia: "06-09-2022",
+    maniana: "Gerometta ",
+    tarde: "Galvan ",
+  },
+  {
+    dia: "07-09-2022",
+    maniana: "Knuttzen ",
+    tarde: "Saucedo ",
+  },
+  {
+    dia: "08-09-2022",
+    maniana: "Galvan ",
+    tarde: "Gerometta ",
+  },
+  {
+    dia: "09-09-2022",
+    maniana: "Saucedo ",
+    tarde: "Knuttzen ",
+  },
+  {
+    dia: "10-09-2022",
+    maniana: "Galvan ",
+    tarde: "Galvan ",
+  },
+  {
+    dia: "11-09-2022",
+    maniana: "Galvan ",
+    tarde: "Galvan ",
+  },
+  {
+    dia: "12-09-2022",
+    maniana: "Saucedo ",
+    tarde: "Gerometta ",
+  },
+  {
+    dia: "13-09-2022",
+    maniana: "Knuttzen ",
+    tarde: "Galvan ",
+  },
+  {
+    dia: "14-09-2022",
+    maniana: "Gerometta Garigliano",
+    tarde: "Saucedo ",
+  },
+  {
+    dia: "15-09-2022",
+    maniana: "Galvan ",
+    tarde: "Garigliano ",
+  },
+  {
+    dia: "16-09-2022",
+    maniana: "Gerometta Saucedo",
+    tarde: "Knuttzen ",
+  },
+  {
+    dia: "17-09-2022",
+    maniana: "Garigliano ",
+    tarde: "Garigliano ",
+  },
+  {
+    dia: "18-09-2022",
+    maniana: "Garigliano ",
+    tarde: "Garigliano ",
+  },
+  {
+    dia: "03-10-2022",
+    maniana: "Galvan Knuttzen",
+    tarde: "Gerometta "
+    },
     {
-      dia: "29-08-2022",
+    dia: "04-10-2022",
+    maniana: "Saucedo ",
+    tarde: "Garigliano "
+    },
+    {
+    dia: "05-10-2022",
+    maniana: "Galvan Gerometta",
+    tarde: "Knuttzen "
+    },
+    {
+    dia: "06-10-2022",
+    maniana: "Garigliano ",
+    tarde: "Saucedo "
+    },
+    {
+    dia: "07-10-2022",
+    maniana: "Gerometta ",
+    tarde: "Gerometta "
+    },
+    {
+    dia: "08-10-2022",
+    maniana: "Gerometta ",
+    tarde: "Gerometta "
+    },
+    {
+    dia: "09-10-2022",
+    maniana: "Gerometta ",
+    tarde: "Gerometta "
+    },
+    {
+      dia: "10-10-2022",
       maniana: "Galvan ",
-      tarde: "Knuttzen ",
-    },
-    {
-      dia: "30-08-2022",
-      maniana: "Saucedo ",
-      tarde: "Gerometta ",
-    },
-    {
-      dia: "31-08-2022",
-      maniana: "Galvan ",
-      tarde: "Knuttzen ",
-    },
-    {
-      dia: "01-09-2022",
-      maniana: "Gerometta ",
-      tarde: "Saucedo ",
-    },
-    {
-      dia: "02-09-2022",
-      maniana: "Knuttzen ",
-      tarde: "Galvan ",
-    },
-    {
-      dia: "03-09-2022",
-      maniana: "Gerometta ",
-      tarde: "Gerometta ",
-    },
-    {
-      dia: "04-09-2022",
-      maniana: "Gerometta ",
-      tarde: "Gerometta ",
-    },
-    {
-      dia: "05-09-2022",
-      maniana: "Knuttzen ",
-      tarde: "Saucedo ",
-    },
-    {
-      dia: "06-09-2022",
-      maniana: "Gerometta ",
-      tarde: "Galvan ",
-    },
-    {
-      dia: "07-09-2022",
-      maniana: "Knuttzen ",
-      tarde: "Saucedo ",
-    },
-    {
-      dia: "08-09-2022",
-      maniana: "Galvan ",
-      tarde: "Gerometta ",
-    },
-    {
-      dia: "09-09-2022",
-      maniana: "Saucedo ",
-      tarde: "Knuttzen ",
-    },
-    {
-      dia: "10-09-2022",
-      maniana: "Galvan ",
-      tarde: "Galvan ",
-    },
-    {
-      dia: "11-09-2022",
-      maniana: "Galvan ",
-      tarde: "Galvan ",
-    },
-    {
-      dia: "12-09-2022",
-      maniana: "Saucedo ",
-      tarde: "Gerometta ",
-    },
-    {
-      dia: "13-09-2022",
-      maniana: "Knuttzen ",
-      tarde: "Galvan ",
-    },
-    {
-      dia: "14-09-2022",
-      maniana: "Gerometta Garigliano",
-      tarde: "Saucedo ",
-    },
-    {
-      dia: "15-09-2022",
-      maniana: "Galvan ",
-      tarde: "Garigliano ",
-    },
-    {
-      dia: "16-09-2022",
-      maniana: "Gerometta Saucedo",
-      tarde: "Knuttzen ",
-    },
-    {
-      dia: "17-09-2022",
-      maniana: "Garigliano ",
-      tarde: "Garigliano ",
-    },
-    {
-      dia: "18-09-2022",
-      maniana: "Garigliano ",
-      tarde: "Garigliano ",
-    },
-    {
-      dia: "03-10-2022",
-      maniana: "Galvan Knuttzen",
-      tarde: "Gerometta "
+      tarde: "Galvan "
       },
       {
-      dia: "04-10-2022",
-      maniana: "Saucedo ",
-      tarde: "Garigliano "
-      },
-      {
-      dia: "05-10-2022",
-      maniana: "Galvan Gerometta",
+      dia: "11-10-2022",
+      maniana: "Gerometta ",
       tarde: "Knuttzen "
       },
       {
-      dia: "06-10-2022",
-      maniana: "Garigliano ",
+      dia: "12-10-2022",
+      maniana: "Galvan Garigliano",
       tarde: "Saucedo "
       },
       {
-      dia: "07-10-2022",
+      dia: "13-10-2022",
       maniana: "Gerometta ",
-      tarde: "Gerometta "
+      tarde: "Galvan "
       },
       {
-      dia: "08-10-2022",
-      maniana: "Gerometta ",
-      tarde: "Gerometta "
+      dia: "14-10-2022",
+      maniana: "Knuttzen Garigliano",
+      tarde: "Saucedo "
       },
       {
-      dia: "09-10-2022",
-      maniana: "Gerometta ",
-      tarde: "Gerometta "
+      dia: "15-10-2022",
+      maniana: "Galvan ",
+      tarde: "Galvan "
       },
       {
-        dia: "10-10-2022",
-        maniana: "Galvan ",
-        tarde: "Galvan "
+      dia: "16-10-2022",
+      maniana: "Galvan ",
+      tarde: "Galvan "
+      },
+      {
+        dia: "26-10-2022",
+        maniana: "Galvan Knuttzen",
+        tarde: "Saucedo"
         },
         {
-        dia: "11-10-2022",
-        maniana: "Gerometta ",
-        tarde: "Knuttzen "
-        },
-        {
-        dia: "12-10-2022",
-        maniana: "Galvan Garigliano",
-        tarde: "Saucedo "
-        },
-        {
-        dia: "13-10-2022",
-        maniana: "Gerometta ",
-        tarde: "Galvan "
-        },
-        {
-        dia: "14-10-2022",
-        maniana: "Knuttzen Garigliano",
-        tarde: "Saucedo "
-        },
-        {
-        dia: "15-10-2022",
-        maniana: "Galvan ",
-        tarde: "Galvan "
-        },
-        {
-        dia: "16-10-2022",
-        maniana: "Galvan ",
-        tarde: "Galvan "
-        },
-  ];
+          dia: "27-10-2022",
+          maniana: "Gerometta",
+          tarde: "Garigliano"
+          },
+];
