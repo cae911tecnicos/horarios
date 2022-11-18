10d70a8f3fbc7a19284b7ddf4aac9af68227a99c
911tecnicos <cae911tecnicos@gmail.com>
2022-11-18 15:55:29 -0300

    Se corrigen horario de feriado 08/12 de informes judiciales.

--- assets/js/horario-manual/hm__sec-informes-judiciales.js
+++ assets/js/horario-manual/hm__sec-informes-judiciales.js
@@ -1,1 +1,14 @@
-export const manual_judiciales = [];
+export const manual_judiciales = [
+    
+    {
+        dia: "08-12-2022",
+        maniana: "Olivera ",
+        tarde: "Olivera ",
+    },
+
+    {
+        dia: "09-12-2022",
+        maniana: "Morello ",
+        tarde: "Morello ",
+    },
+];
